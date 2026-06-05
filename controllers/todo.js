c4065a2eff88742e1a88e8815fd812e4c9bf8293
tamarazu <tamarazulaika.u@gmail.com>
2026-06-05 22:13:46 +0700

    updating stack

--- controllers/todo.js
+++ controllers/todo.js
@@ -1,87 +1,81 @@
-const { Todo } = require('../models')
+const { Todo } = require("../models");
 
 class TodoController {
-    static create(req, res, next) {
-        console.log('masuk sini loh udaaaah')
-        const { title, description, status, due_date } = req.body
-        const newTodo = { title, description, status, due_date }
-        newTodo.UserId = req.currentUserId
-        Todo
-            .create(newTodo)
-            .then(todo => {
-                res.status(201).json(todo)
-            })
-            .catch(next)
-    }
-    static findAll (req, res, next) {
-        Todo
-            .findAll({
-                where: {
-                    UserId : req.currentUserId
-                }
-            })
-            .then(todos => {
-                res.status(200).json(todos)
-            })
-            .catch(next)
-    }
-    static findOne (req, res, next) {
-        let id = req.params.id
-        Todo
-            .findOne({
-                where: {
-                    id: id
-                }
-            })
-            .then(todo => {
-                res.status(200).json(todo)
-            })
-            .catch(next)
-    }
-    static update (req, res, next) {
-        let id = req.params.id
-        const { title, description, status, due_date } = req.body
-        const updateTodo = { title, description, status, due_date }
-        Todo
-            .update(updateTodo, {
-                where: {
-                    id : id
-                }
-            })
-            .then(todo => {
-                return Todo.findOne({
-                    where: {
-                        id : id
-                    }
-                })
-            })
-            .then(todo => {
-                res.status(200).json(todo)
-            })
-            .catch(next)
-    }
-    static remove (req, res, next) {
-        let id = req.params.id
-        let todoDeleted
-        Todo
-            .findOne({
-                where: {
-                    id : id
-                }
-            })
-            .then(todo => {
-                todoDeleted = todo
-                return Todo.destroy({
-                    where: {
-                        id : id
-                    }
-                })
-            })  
-            .then(todo => {
-                res.status(200).json(todoDeleted)
-            })
-            .catch(next)
-    }
+  static create(req, res, next) {
+    const { title, description, status, due_date } = req.body;
+    const newTodo = { title, description, status, due_date };
+    newTodo.UserId = req.currentUserId;
+    Todo.create(newTodo)
+      .then((todo) => {
+        res.status(201).json(todo);
+      })
+      .catch(next);
+  }
+  static findAll(req, res, next) {
+    Todo.findAll({
+      where: {
+        UserId: req.currentUserId,
+      },
+    })
+      .then((todos) => {
+        res.status(200).json(todos);
+      })
+      .catch(next);
+  }
+  static findOne(req, res, next) {
+    let id = req.params.id;
+    Todo.findOne({
+      where: {
+        id: id,
+      },
+    })
+      .then((todo) => {
+        res.status(200).json(todo);
+      })
+      .catch(next);
+  }
+  static update(req, res, next) {
+    let id = req.params.id;
+    const { title, description, status, due_date } = req.body;
+    const updateTodo = { title, description, status, due_date };
+    Todo.update(updateTodo, {
+      where: {
+        id: id,
+      },
+    })
+      .then((todo) => {
+        return Todo.findOne({
+          where: {
+            id: id,
+          },
+        });
+      })
+      .then((todo) => {
+        res.status(200).json(todo);
+      })
+      .catch(next);
+  }
+  static remove(req, res, next) {
+    let id = req.params.id;
+    let todoDeleted;
+    Todo.findOne({
+      where: {
+        id: id,
+      },
+    })
+      .then((todo) => {
+        todoDeleted = todo;
+        return Todo.destroy({
+          where: {
+            id: id,
+          },
+        });
+      })
+      .then((todo) => {
+        res.status(200).json(todoDeleted);
+      })
+      .catch(next);
+  }
 }
 
-module.exports = TodoController
+module.exports = TodoController;
--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,11 +1,10 @@
 module.exports = (err, req, res, next) => {
-    if(err.name === 'SequelizeValidationError') {
-        const message = err.errors.map( el => el.message)
-        console.log('ERROR MASUK SINIIII==============')
-        res.status(400).json({
-            errors : message
-        })
-    } else {
-        res.status(err.status || 500).json(err.message || 'Internal Server Error')
-    }
-}
+  if (err.name === "SequelizeValidationError") {
+    const message = err.errors.map((el) => el.message);
+    res.status(400).json({
+      errors: message,
+    });
+  } else {
+    res.status(err.status || 500).json(err.message || "Internal Server Error");
+  }
+};
